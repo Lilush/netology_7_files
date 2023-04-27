30201fd8bf52664c033aef3f3556f87e3e88f797
Anton Veshkin <antonveshkin@gmail.com>
2023-04-28 00:53:18 +0300

    Add fucntion that calculate ingredients for some dishes. Task 7.2

--- main.py
+++ main.py
@@ -10,11 +10,24 @@ def parse_recipes(file_path):
                 name, quantity, measure = temp_data.split('|')
                 ingredients.append(
                     {'ingredient_name' : name.strip(), 
-                     'quantity' : quantity.strip(), 
+                     'quantity' : int(quantity.strip()), 
                      'measure' : measure.strip()})
             cook_book[dish_name] = ingredients
             file.readline()
         return cook_book
+    
+def get_shop_list_by_dishes(cook_book, dishes, person_count):
+    ingredients = {}
+    for dish in dishes:
+        if dish not in cook_book:
+            return "Рецепт не найден"
+        for ingredient in cook_book[dish]:
+            ingradient_name = ingredient['ingredient_name']
+            measure = ingredient['measure']
+            quantity = ingredient['quantity']
+            ingredients.setdefault(ingradient_name, {'measure': measure, 'quantity': 0})
+            ingredients[ingradient_name]['quantity'] += quantity * person_count
+    return ingredients
             
 
 
